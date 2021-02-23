1e0a311a13b4c3ccdbda774803f8d9e9074711ab
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-02-23 18:27:07 +0330

    Add lotttie

--- components/Assignments.vue
+++ components/Assignments.vue
@@ -2,7 +2,7 @@
   <v-row justify="center" align="center" class="mt-4">
     <v-col cols="1"/>
     <v-col cols="10" class="justify-center">
-      <v-list subheader>
+      <v-list v-if="assignments.length !== 0" subheader>
         <v-list-item
           v-for="(hw, index) in assignments"
           :style="index!==0 ? 'border-top: solid #E0E0E0 1px' : ''"
@@ -10,7 +10,7 @@
           <v-list-item-avatar>
             <v-icon color="blue">mdi-book</v-icon>
           </v-list-item-avatar>
-          <v-list-item-title :class="`${textSize.assignmentText}`" v-text="hw.name"/>
+          <v-list-item-title :class="`${sizes.assignmentText}`" v-text="hw.name"/>
           <v-list-item-action>
             <v-btn text>
               <v-icon class="mr-2" color="green">mdi-download</v-icon>
@@ -18,35 +18,53 @@
           </v-list-item-action>
         </v-list-item>
       </v-list>
+      <div v-else>
+        <lottie class="mt-8" :width="sizes.lottieSize" :options="searchingLottie"/>
+        <div class="text-center text-body-1">Nothing to show</div>
+      </div>
     </v-col>
     <v-col cols="1"/>
   </v-row>
 </template>
 
 <script>
+  import lottie from 'vue-lottie/src/lottie';
+  import  * as  searchingLottie from '../static/lottie/searching.json'
+
   export default {
     name: "Assignments",
+    components: {
+      lottie
+    },
+    data: () => ({
+      searchingLottie: { animationData: searchingLottie.default }
+    }),
     computed: {
-      textSize() {
+      sizes() {
         switch (this.$vuetify.breakpoint.name) {
           case 'xs':
             return {
+              lottieSize: 300,
               assignmentText: 'text-caption',
             };
           case 'sm':
             return {
+              lottieSize: 300,
               assignmentText: 'text-body-1',
             };
           case 'md':
             return {
+              lottieSize: 300,
               assignmentText: 'text-body-1',
             };
           case 'lg':
             return {
+              lottieSize: 300,
               assignmentText: 'text-body-1',
             };
           case 'xl':
             return {
+              lottieSize: 300,
               assignmentText: 'text-body-1',
             };
         }
